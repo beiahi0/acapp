ccd84154cdc23b5222b863e31fe03384e5e9be71
beiahi0 <1666825283@qq.com>
2023-09-30 15:24:16 +0800

    write menu and playground scene

--- acapp/scripts/compress_game_js.sh
+++ acapp/scripts/compress_game_js.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-JS_PATH=/home/acs/acapp/game/static/js/
+# JS_PATH=/home/jackson/code/py/acapp/game/static/js/
+JS_PATH=/home/jackson/code/py/Django/acapp/game/static/js/
+
 JS_PATH_DIST=${JS_PATH}dist/
 JS_PATH_SRC=${JS_PATH}src/
 
